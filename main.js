e0cbd9f42f331d8bacbfd4d0c60d15e0c02158b6
semeniuking <mikhail.semenyuk.devmrks@gmail.com>
2022-01-12 15:11:32 +0200

    disable wrong answer when clicked on right

--- main.js
+++ main.js
@@ -7,23 +7,23 @@ function getById(id) {
 }
 
 function showNextStep(element) {
-    var value = document.getElementById(element).getAttribute('id');
-    var nextStep = getById('' + value.substr(0, value.length - 8) + '');
+    let value = document.getElementById(element).getAttribute('id');
+    let nextStep = getById('' + value.substr(0, value.length - 8) + '');
     nextStep.style.display = 'flex';
 }
 
 
 function removeBlocks() {
     const questions = [...document.querySelectorAll('.questions')];
-    for(let i = 1; i < questions.length; i++){
+    for (let i = 1; i < questions.length; i++) {
         questions[i].style.display = 'none';
     }
 }
 
 function removeStyles() {
     const allques = [...document.querySelectorAll('.question')];
-    for(let i = 0; i < allques.length; i++){
-        allques[i].classList.remove('false','true')
+    for (let i = 0; i < allques.length; i++) {
+        allques[i].classList.remove('false', 'true')
     }
 }
 
@@ -34,7 +34,6 @@ startOverBtn.addEventListener('click', (target) => {
     removeStyles();
     removeBlocks();
 })
-
 
 document.addEventListener('click', ({target}) => {
     if (!event.target.classList.contains('question')) return;
@@ -47,6 +46,31 @@ document.addEventListener('click', ({target}) => {
         question.classList.add('true');
         score++;
     }
+
+    let container = target.closest('.questions');
+    let check = container.querySelectorAll('.question');
+    // let trueQ = check[i].getAttribute('data-question') === 'true';
+    for (i = 0; i < check.length; i++) {
+        if (target.getAttribute('data-question') === 'false' && check[0].getAttribute('data-question') === 'false') {
+        } else if (target.getAttribute('data-question') === 'true' && check[1].getAttribute('data-question') === 'false') {
+            check[1].classList.add('disable')
+        } else if(target.getAttribute('data-question') === 'true' && check[0].getAttribute('data-question') === 'false'){
+            check[0].classList.add('disable')
+        }
+    }
 });
+
+// if (target.getAttribute('data-question') === 'false') {
+//
+// } else if (target.getAttribute('data-question') === 'true') {
+// }
+
+
+// if (check[1].getAttribute('data-question') === 'true') {
+// } else if(check[0].getAttribute('data-question') === 'false'){
+//     check[0].classList.add('disable');
+// } else{
+//     check[1].classList.add('disable');
+// }
 
 
